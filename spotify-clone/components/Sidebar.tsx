25ec134a2aa6ef4f90567fb23c03c799df17e3c0
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-04 22:57:01 +0300

    Update Sidebar.tsx

--- spotify-clone/components/Sidebar.tsx
+++ spotify-clone/components/Sidebar.tsx
@@ -36,7 +36,9 @@ const Sidebar: React.FC<SidebarProps> = ({
     <div className="flex h-full">
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-fuull w-[300px] p-2">
         <Box>
-          Sidebar Navigation
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            
+          </div>
         </Box>
         <Box className="overflow-y-auto h-full">
           Song Library
