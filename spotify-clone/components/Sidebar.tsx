61874c62dc06ed9561bfd0211506e080de2548e1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-04 22:59:51 +0300

    Update Sidebar.tsx

--- spotify-clone/components/Sidebar.tsx
+++ spotify-clone/components/Sidebar.tsx
@@ -37,7 +37,12 @@ const Sidebar: React.FC<SidebarProps> = ({
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-fuull w-[300px] p-2">
         <Box>
           <div className="flex flex-col gap-y-4 px-5 py-4">
-            
+            {rouutes.map((item) => (
+              <SidebarItem 
+                key={item.label}
+                {...item}
+              />
+            ))}
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
