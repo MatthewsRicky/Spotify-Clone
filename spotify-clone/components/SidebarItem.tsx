d38c4bd22baaeb166a8f061a1bd9f90e0862291d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-04 23:09:18 +0300

    Update SidebarItem.tsx

--- spotify-clone/components/SidebarItem.tsx
+++ spotify-clone/components/SidebarItem.tsx
@@ -19,7 +19,21 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
     <Link 
     href={href}
     className={twMerge(`
-    `)}>
+      flex
+      flex-ro
+      h-auto
+      items-center
+      w-full
+      gap-x-4
+      text-md
+      font-medium
+      cursor-pointer
+      hover:text-white
+      transition
+      text-neutral-400
+      py-1
+    `)}
+    >
       Sidebar Item
     </Link>
   )
