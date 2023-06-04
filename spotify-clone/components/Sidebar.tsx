ffa7a5eaa3e8adf6d640d29f984c5ee3d89d6e79
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-04 22:48:21 +0300

    Update Sidebar.tsx

--- spotify-clone/components/Sidebar.tsx
+++ spotify-clone/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { BiSearch } from "react-icons/bi"
 
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
+import Box from "./Box";
 
 interface SidebarProps {
   children: React.ReactNode
@@ -32,8 +33,12 @@ const Sidebar: React.FC<SidebarProps> = ({
   ], [pathname])
 
   return(
-    <div>
-      {children}
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-fuull w-[300px] p-2">
+        <Box>
+          Sidebar Navigation
+        </Box>
+      </div>
     </div>
   )
 }
