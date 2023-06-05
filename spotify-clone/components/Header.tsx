d680eecaf9ddcdb47b095982d118ed97eb7e0a4f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-05 09:20:44 +0300

    Update Header.tsx

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -1,5 +1,13 @@
 
-const Header = () => {
+interface HeaderProps{
+  children: React.ReactNode
+  className?: string
+}
+
+const Header: React.FC<HeaderProps> = ({
+  children,
+  className
+}) => {
   return (
     <div>Header</div>
   )
