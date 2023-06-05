138fd05f5dd5930d6b34fb0801beaf465e89f76e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-05 09:24:49 +0300

    Update Header.tsx

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
 
 interface HeaderProps{
   children: React.ReactNode
@@ -8,8 +12,23 @@ const Header: React.FC<HeaderProps> = ({
   children,
   className
 }) => {
+const router = useRouter();
+
+const handleLogout = () => {
+  //handle log out in the future
+}
+
   return (
-    <div>Header</div>
+    <div
+      className={twMerge(`
+        h-fit
+        bg-gradient-to-b
+        from-emerald-800
+        p-6
+      `)}
+    >
+
+    </div>
   )
 }
 
