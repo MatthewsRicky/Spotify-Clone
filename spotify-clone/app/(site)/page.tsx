0dbdec13317820bc59703f72868c8b8b53b34761
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-06 18:00:30 +0300

    Update page.tsx

--- spotify-clone/app/(site)/page.tsx
+++ spotify-clone/app/(site)/page.tsx
@@ -22,7 +22,9 @@ export default function Home() {
             Welcome back
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-4-cols mt-4">
-            <ListItem />
+            <ListItem 
+              image="/images/liked.png"
+            />
 
           </div>
         </div>
