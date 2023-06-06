bf437de33fa2ca7cce1297176c8fc843a2d4b406
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-06 11:58:59 +0300

    Update Header.tsx

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import { RxCaretLeft } from "react-icons/rx";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 
 interface HeaderProps{
   children: React.ReactNode
@@ -33,6 +33,12 @@ const handleLogout = () => {
         <div className="hidden md:flex gap-x-2 items-center">
           <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
             <RxCaretLeft 
+              size={35}
+              className="text-white"
+            />
+          </button>
+          <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+            <RxCaretRight 
               size={35}
               className="text-white"
             />
