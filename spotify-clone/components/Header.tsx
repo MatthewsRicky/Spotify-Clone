36c86c65a39e0a9d39efe68b7aabd699960ee997
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-06 12:11:05 +0300

    Update Header.tsx

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -6,6 +6,8 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 
+import Button from "./Button";
+
 interface HeaderProps{
   children: React.ReactNode
   className?: string
