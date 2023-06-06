30a68bd78a1420663fd33b549182cfb05ece32a1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-06 17:43:44 +0300

    Update ListItem.tsx

--- spotify-clone/components/ListItem.tsx
+++ spotify-clone/components/ListItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Liked from "../public/images/liked.png"
 
 interface ListItemProps {
   image: string;
@@ -17,7 +18,10 @@ const ListItem: React.FC<ListItemProps> = ({
     <button>
       <div className="reelativ group flex iteems-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover/:bg-neutral-100/20 transition pr-4">
         <div className="relative min-h-[64] min-w-[64]">
-          <Image />
+          <Image 
+            src={Liked}
+            alt="liked"
+          />
         </div>
       </div>
     </button>
