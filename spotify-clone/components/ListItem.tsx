27fd29d71d842814e280f0043861f3abdf71b7cd
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-07 19:43:48 +0300

    Update ListItem.tsx

--- spotify-clone/components/ListItem.tsx
+++ spotify-clone/components/ListItem.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useRouter } from "next/navigation";
+
 interface ListItemProps {
   image: string;
   name: string;
@@ -11,6 +13,14 @@ const ListItem: React.FC<ListItemProps> = ({
   name,
   href
 }) => {
+
+  const router = useRouter();
+
+  const onClick = () => {
+    //add authentication before push
+    router.push(href);
+  }
+
   return (
     <div>
       List Item
