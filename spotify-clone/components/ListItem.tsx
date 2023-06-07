565b2ef8498a927300316dd43eb2f9e68183396b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-07 19:41:56 +0300

    Update ListItem.tsx

--- spotify-clone/components/ListItem.tsx
+++ spotify-clone/components/ListItem.tsx
@@ -1,4 +1,16 @@
-const ListItem = () => {
+"use client"
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({
+  image,
+  name,
+  href
+}) => {
   return (
     <div>
       List Item
