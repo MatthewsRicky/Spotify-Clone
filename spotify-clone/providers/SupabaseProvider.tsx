975622cacfff251c709be4fe992dfedc7866f506
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-07 22:22:10 +0300

    Update SupabaseProvider.tsx

--- spotify-clone/providers/SupabaseProvider.tsx
+++ spotify-clone/providers/SupabaseProvider.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { useState } from "react";
+
 import { Database } from "../types_db";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 interface  SupabaseProviderProps {
 children: React.ReactNode
@@ -11,9 +13,15 @@ children: React.ReactNode
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
   children
 }) => {
-  const [superbaseClient] = useState(() => {
+  const [supabaseClient] = useState(() => {
     createClientComponentClient<Database>()
   })
+
+  return (
+    <SessionContextProvider supabasClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  )
 }
 
-export default SupabaseProvider
+export default SupabaseProvider;
