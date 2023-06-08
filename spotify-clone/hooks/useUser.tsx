950fe8fd47e16215ecb733e97d1ad79f9d203d76
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-08 23:42:01 +0300

    Update useUser.tsx

--- spotify-clone/hooks/useUser.tsx
+++ spotify-clone/hooks/useUser.tsx
@@ -3,7 +3,7 @@ import {
   useSessionContext,
   useUser as useSupaUser 
 } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useContext } from "react";
 
 import { Subscription, UserDetails } from "../types"
 
@@ -79,3 +79,10 @@ export const MyUserContextProvider = (props: Props) => {
 
   return <UserContext.Provider value={value} {...props} />
 };
+
+export const useUser = () => {
+  const context = useContext(UserContext);;
+  if (context === undefined) {
+    throw new Error('useUser must be used within a MyUserContextProvider');
+  }
+}
