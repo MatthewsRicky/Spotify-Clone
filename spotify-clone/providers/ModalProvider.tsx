f2c663038c09fc2df66542b9cc93356948035ad7
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-01 15:52:48 +0300

    Update ModalProvider.tsx

--- spotify-clone/providers/ModalProvider.tsx
+++ spotify-clone/providers/ModalProvider.tsx
@@ -1,6 +1,18 @@
 "use client";
 
+import { useState } from "react";
+
 const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       Modals!
