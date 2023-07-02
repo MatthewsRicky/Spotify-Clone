6744b7a7ebefde0f8765030dfa531c3a38e5c678
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-02 15:57:18 +0300

    Update Modal.tsx

--- spotify-clone/components/Modal.tsx
+++ spotify-clone/components/Modal.tsx
@@ -32,7 +32,7 @@ const Modal: React.FC<ModalProps> = ({
         "
         />
         <Dialog.Content className="
-          fixed drop-shadow border border-neutral-700 top-[50%] left-[50%] max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] translate-x-[-50%] translate-y-[50%] rounded-md bg-neutral-800 p-[25px] focus:outline-none 
+          fixed drop-shadow border border-neutral-700 top-[50%] left-[50%] max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-neutral-800 p-[25px] focus:outline-none 
         ">
           <Dialog.Title className="text-xl text-center font-bold mb-4">
             {title}
@@ -43,17 +43,15 @@ const Modal: React.FC<ModalProps> = ({
           <div>
             {children}
           </div>
-          <div>
-            <Dialog.Close asChild>
-              <button className="text-neutral-400 hover:text-white absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:outline">
-                <IoMdClose />
-              </button>
-            </Dialog.Close>
-          </div>
+          <Dialog.Close asChild>
+            <button className="text-neutral-400 hover:text-white absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:outline-none">
+              <IoMdClose />
+            </button>
+          </Dialog.Close>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   )
 }
 
-export default Modal
+export default Modal;
