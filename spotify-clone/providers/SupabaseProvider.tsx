e793b52861c14839d8535ce5dcaafe71a6e02045
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-08 03:13:08 +0300

    Update SupabaseProvider.tsx

--- spotify-clone/providers/SupabaseProvider.tsx
+++ spotify-clone/providers/SupabaseProvider.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { SupabaseProvider } from '@supabase/auth-helpers-nextjs';
+
 
 import { Database } from "../types_db";
 
