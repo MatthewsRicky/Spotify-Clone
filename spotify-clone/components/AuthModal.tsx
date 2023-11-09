ca9809289b4f319546a5861e8a950354d0fff144
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-09 18:22:34 +0300

    Update AuthModal.tsx

--- spotify-clone/components/AuthModal.tsx
+++ spotify-clone/components/AuthModal.tsx
@@ -1,23 +1,44 @@
 "use client";
 
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useSessionContext, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 import Modal from './Modal'
 import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { useRouter } from 'next/navigation';
+import useeAuthModal from '../hooks/useAuthModal';
 
 const AuthModal = () => {
 
   const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const {session} = useSessionContext();
+  const {onClose, isOpen} = useeAuthModal();
 
   return (
     <Modal
       title="Welcome Back"
       description="Login to your account"
-      isOpen
+      isOpen= {isOpen}
       onChange={() => {}}
     >
      <Auth
-     supabaseClient={supabaseClient} 
+      theme="dark"
+      providers={['github']}
+      magicLink
+     supabaseClient={supabaseClient}
+     appearance={{
+     
+      theme: ThemeSupa,
+      variables: {
+        default: {
+          colors: {
+            brand: "#404040",
+            brandAccent: "#22c55e"
+          }
+        }
+      }
+     }} 
      />
     </Modal>
   )
