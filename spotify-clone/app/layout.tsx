04344c9a17454f3c50f41c56c2900b16890f90ef
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-10 21:44:14 +0300

    Toast provider

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -1,33 +1,31 @@
-
-import Sidebar from '../components/Sidebar'
-import ModalProvider from '../providers/ModalProvider'
-import SupabaseProvider from '../providers/SupabaseProvider'
-import UserProvider from '../providers/UserProvider'
-import './globals.css'
-
+import Sidebar from "../components/Sidebar";
+import ModalProvider from "../providers/ModalProvider";
+import SupabaseProvider from "../providers/SupabaseProvider";
+import ToasterProvider from "../providers/ToasterProvider";
+import UserProvider from "../providers/UserProvider";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen to Music',
-}
+	title: "Spotify Clone",
+	description: "Listen to Music",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>
-        <SupabaseProvider>
-          <UserProvider>
-            <ModalProvider />
-            <Sidebar>
-            {children}
-          </Sidebar>
-          </UserProvider> 
-        </SupabaseProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body>
+				<ToasterProvider />
+				<SupabaseProvider>
+					<UserProvider>
+						<ModalProvider />
+						<Sidebar>{children}</Sidebar>
+					</UserProvider>
+				</SupabaseProvider>
+			</body>
+		</html>
+	);
 }
