1fd6f8ef8ca5224c46b1c9935d1d105b8a33ff3a
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-12 22:01:56 +0300

    Update Library.tsx

--- spotify-clone/components/Library.tsx
+++ spotify-clone/components/Library.tsx
@@ -1,33 +1,42 @@
- "use client"
+"use client";
 
- import { TbPlaylist} from "react-icons/tb";
- import { AiOutlinePlus } from "react-icons/ai";
- 
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "../hooks/useAuthModal";
+import { useUser } from "../hooks/useUser";
+import toast from "react-hot-toast";
+import useUploadModal from "../hooks/useUploadModal";
 
-  const Library = () => {
-    const onClick = () => {
-    //handle upload later
-  };
-  return (
-    <div className="flex flex-col">
-      <div className="flex items-center justify-between px-5 pt-4">
-        <div className="inline-flex items-center gap-x-2">
-          <TbPlaylist className="text-neutral-500" size={26} />
-          <p className="text-neutral-400 font-medium text-md">
-            Your Library
-          </p>
-        </div>
-        <AiOutlinePlus 
-          onClick={onClick}
-          size={20}
-          className="text-neutral-400 cursor-pointer hover:text-white transition"
-        />
-      </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">
-        List of songs
-      </div>
-    </div>
-  )
-}
+const Library = () => {
+	const authModal = useAuthModal();
+	const uploadModal = useUploadModal();
 
-export default Library
+	const { user } = useUser();
+	const onClick = () => {
+		//if (!user) {
+		//return authModal.onOpen();
+		return uploadModal.onOpen();
+	};
+
+	return (
+		<div className='flex flex-col'>
+			<div className='flex items-center justify-between px-5 pt-4'>
+				<div className='inline-flex items-center gap-x-2'>
+					<TbPlaylist
+						className='text-neutral-500'
+						size={26}
+					/>
+					<p className='text-neutral-400 font-medium text-md'>Your Library</p>
+				</div>
+				<AiOutlinePlus
+					onClick={onClick}
+					size={20}
+					className='text-neutral-400 cursor-pointer hover:text-white transition'
+				/>
+			</div>
+			<div className='flex flex-col gap-y-2 mt-4 px-3'>List of songs</div>
+		</div>
+	);
+};
+
+export default Library;
