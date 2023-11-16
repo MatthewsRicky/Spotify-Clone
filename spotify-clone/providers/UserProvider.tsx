ebac115df943b24e3be511100f7bf3dfdc42a142
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-16 23:12:12 +0300

    Update UserProvider.tsx

--- spotify-clone/providers/UserProvider.tsx
+++ spotify-clone/providers/UserProvider.tsx
@@ -3,17 +3,11 @@
 import { MyUserContextProvider } from "../hooks/useUser";
 
 interface UserProviderProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-const UserProvider: React.FC<UserProviderProps> = ({
-  children
-}) => {
-  return (
-    <MyUserContextProvider>
-      {children}
-    </MyUserContextProvider>
-  )
-}
+const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+	return <MyUserContextProvider>{children}</MyUserContextProvider>;
+};
 
 export default UserProvider;
