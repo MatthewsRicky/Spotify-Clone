774ce1499feec18edb31814b8d1a39e22ce7f6b1
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-17 18:55:44 +0300

    Update PageContent.tsx

--- spotify-clone/app/(site)/components/PageContent.tsx
+++ spotify-clone/app/(site)/components/PageContent.tsx
@@ -10,7 +10,7 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
 	if (songs.length === 0) {
 		return <div className='mt-4 text-neutral-400'>No songs Available!</div>;
 	}
-	return <div>Page Content!</div>;
+	return <div className='grid grid-cols-2 sm:grid-cols-3'>Page Content!</div>;
 };
 
 export default PageContent;
