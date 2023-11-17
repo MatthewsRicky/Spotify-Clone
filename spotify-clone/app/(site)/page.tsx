68d7151d5d4948cd26b9e81341458e6240a6f88a
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-17 15:19:25 +0300

    Update page.tsx

--- spotify-clone/app/(site)/page.tsx
+++ spotify-clone/app/(site)/page.tsx
@@ -1,46 +1,45 @@
 import { Figtree } from "next/font/google";
 import Header from "@/src/components/Header";
 import ListItem from "@/src/components/ListItem";
+import getSongs from "@/src/actions/getSongs";
 
+const font = Figtree({ subsets: ["latin"] });
 
-const font = Figtree({subsets: ['latin']})
+export default async function Home() {
+	const songs = await getSongs();
 
-export default function Home() {
-  return (
-    <div className="
+	return (
+		<div
+			className='
       bg-neutral-900
       rounded-lg
       h-full
       w-full
       overflow-x-hidden
       overflow-y-auto
-      ">
-       <Header>
-        <div className="mb-2">
-          <h1
-            className="text-white text-3xl font-semobold"
-          >
-            Welcome back
-          </h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem 
-              image="/images/liked.png"
-              name="Liked Songs"
-              href="liked"
-            />
-          </div>
-        </div>
-       </Header>
-       <div className="mt-2 mb-7 px-6">
-        <div className="flex justify-between item-center">
-          <h1 className="font-semibold text-2xl text-white">
-            Newest Songs
-          </h1>
-        </div>
-        <div>
-          List of Songs
-        </div>
-       </div>
-    </div>
-  )
+      '>
+			<Header>
+				<div className='mb-2'>
+					<h1 className='text-white text-3xl font-semobold'>Welcome back</h1>
+					<div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4'>
+						<ListItem
+							image='/images/liked.png'
+							name='Liked Songs'
+							href='liked'
+						/>
+					</div>
+				</div>
+			</Header>
+			<div className='mt-2 mb-7 px-6'>
+				<div className='flex justify-between item-center'>
+					<h1 className='font-semibold text-2xl text-white'>Newest Songs</h1>
+				</div>
+				<div>
+					{songs.map((song) => (
+						<div>{song.title}</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
