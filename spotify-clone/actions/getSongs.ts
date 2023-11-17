2d240b37a0b0a550d7c1727a83dfc64d829c2889
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-17 14:34:38 +0300

    Update getSongs.ts

--- spotify-clone/actions/getSongs.ts
+++ spotify-clone/actions/getSongs.ts
@@ -1,3 +1,14 @@
 import { Song } from "@/src/types";
 
-const getSongs = async (): Promise<Song[]> => {};
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+const getSongs = async (): Promise<Song[]> => {
+	const supabase = createServerComponentClient({
+		cookies: cookies,
+	});
+
+	const { data, error } = await supabase
+		.from("songs")
+		.select("*")
+		.order("created_at", { ascending: false });
+};
